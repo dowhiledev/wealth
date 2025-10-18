90b9966215df25ddfbed87f6ee9ee12943aec68f
chandralegend <irugalbandarachandra@gmail.com>
2025-10-18 20:50:48 +0530

    Add chart and report generation commands, enhance PDF reporting, and update dependencies

--- src/wealth/__init__.py
+++ src/wealth/__init__.py
@@ -170,11 +170,15 @@ from .cli.account import app as account_app
 from .cli.tx import app as tx_app
 from .cli.import_cmd import app as import_app
 from .cli.export_cmd import app as export_app
+from .cli.chart import app as chart_app
+from .cli.report import app as report_app
 
 app.add_typer(account_app, name="account")
 app.add_typer(tx_app, name="tx")
 app.add_typer(import_app, name="import")
 app.add_typer(export_app, name="export")
+app.add_typer(chart_app, name="chart")
+app.add_typer(report_app, name="report")
 
 
 portfolio_app = typer.Typer(help="Portfolio views")
